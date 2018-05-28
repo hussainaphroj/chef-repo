b6d0fa42eab1305d5d47a69ab641238afd10d7b1
hussainaphroj <hussainaphroj@gmail.com>
2018-05-28 13:19:18 +0000

    used data bags in template file

--- cookbooks/httpd/recipes/config.rb
+++ cookbooks/httpd/recipes/config.rb
@@ -1,5 +1,15 @@
-cookbook_file "/var/www/html/index.html" do
-  source "index.html"
-  mode "0644"
-  notifies :restart, 'service[httpd]'
+#cookbook_file "/var/www/html/index.html" do
+#  source "index.html"
+#  mode "0644"
+#  notifies :restart, 'service[httpd]'
+#end
+#
+admins = data_bag('admins')
+admin = data_bag_item('admins', 'hussain')
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables(
+  :admins => admins,
+  :admin  => admin
+  ) 
 end
